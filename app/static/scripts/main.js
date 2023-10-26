38689cfe5fbd366b9cd2613b959b73888410465b
Colin Choquette <cwc2075@uncw.edu>
2023-10-25 22:45:25 -0400

    testing with the addbook page

---
+++ app/static/scripts/main.js
@@ -0,0 +1,5 @@
+var add_book_button = document.getElementById("add_book_button");
+
+add_book_button.addEventListener("click", function (){
+    window.location.href = "/templates/add_book.html";
+})
