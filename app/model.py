3eb80abb867b376994d39b85eb03ef61a862f1b7
Ian <fastrunner11.5@gmail.com>
2023-10-29 18:34:48 -0400

    Initial commit

--- app/model.py
+++ app/model.py
@@ -1,23 +1,43 @@
 from app import db
 from datetime import datetime
 
-checkouts = db.Table('checkouts',
-                     db.Column('student_id', db.Integer, db.ForeignKey('student.id'), primary_key=True),
-                     db.Column('book_id', db.Integer, db.ForeignKey('book.id'), primary_key=True),
-                     db.Column('checkout_date', db.DateTime, default=datetime.utcnow)
-                     )
+class Patron(db.Model):
+    patronID = db.Column(db.SmallInteger, primary_key=True)
+    firstName = db.Column(db.String(20))
+    lastName = db.Column(db.String(20))
+    email = db.Column(db.String(30))
+    phoneNum = db.Column(db.String(10))
+    acctBalance = db.Column(db.Numeric(10, 2))
+    itemsRented = db.Column(db.SmallInteger)
+    checkouts = db.relationship('Checkout', backref='patron', lazy='dynamic')
 
+class ItemType(db.Model):
+    typeID = db.Column(db.SmallInteger, primary_key=True)
+    typeName = db.Column(db.String(20))
+    rentDuration = db.Column(db.Integer)
+    items = db.relationship('Item', backref='item_type', lazy='dynamic')
 
-class Student(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(100), unique=True, nullable=False)
-    # Show a many to many
-    books = db.relationship('Book', secondary=checkouts, lazy='subquery',
-                            backref=db.backref('students', lazy=True))
+class Item(db.Model):
+    itemID = db.Column(db.SmallInteger, primary_key=True)
+    itemTitle = db.Column(db.String(50))
+    itemAuthor = db.Column(db.String(50))
+    publishDate = db.Column(db.Date)
+    itemBranch = db.Column(db.String(50))
+    typeID = db.Column(db.SmallInteger, db.ForeignKey('item_type.typeID'))
+    checkouts = db.relationship('Checkout', backref='item', lazy='dynamic')
+    authors = db.relationship('Author', secondary='item_authors')
 
+class Checkout(db.Model):
+    patronID = db.Column(db.SmallInteger, db.ForeignKey('patron.patronID'), primary_key=True)
+    itemID = db.Column(db.SmallInteger, db.ForeignKey('item.itemID'), primary_key=True)
+    checkoutID = db.Column(db.SmallInteger, primary_key=True)
+    returnDate = db.Column(db.Date)
 
-class Book(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    author = db.Column(db.String(100))
+class Author(db.Model):
+    authorID = db.Column(db.SmallInteger, primary_key=True)
+    firstName = db.Column(db.String(20))
+    lastName = db.Column(db.String(20))
+
+class ItemAuthors(db.Model):
+    authorID = db.Column(db.SmallInteger, db.ForeignKey('author.authorID'), primary_key=True)
+    itemID = db.Column(db.SmallInteger, db.ForeignKey('item.itemID'), primary_key=True)
